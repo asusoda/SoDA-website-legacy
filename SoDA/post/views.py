1a580715deda761bb07cba735e094af1f513c1e7
Thomas F <taforres@asu.edu>
2015-07-20 19:02:27 -0700

    Organize templates and static folders

--- SoDA/post/views.py
+++ SoDA/post/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-	return render(request,'index.html')
+	return render(request,'post/index.html')
 def announcements(request):
-	return render(request,'announcements.html')
+	return render(request,'post/announcements.html')
 def competitions(request):
-	return render(request,'competitions.html')
+	return render(request,'post/competitions.html')
 def calendar(request):
-	return render(request,'calendar.html')
+	return render(request,'post/calendar.html')
 def aboutus(request):
-	return render(request,'about-us.html')
+	return render(request,'post/about-us.html')
