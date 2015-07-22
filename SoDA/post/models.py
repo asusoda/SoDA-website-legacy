283a38f540be9238d310af8ecbb4c876fd19093e
Thomas F <taforres@asu.edu>
2015-07-21 17:11:39 -0700

    Create models Competition and Announcemnt

--- SoDA/post/models.py
+++ SoDA/post/models.py
@@ -14,8 +14,28 @@ class PostInfo(models.Model):
 
 	location_boolean = models.BooleanField(default=False,verbose_name="Will this post have a location")
 	#Test ImageField.height_field/.width_field for the material cards
-	image = models.ImageField()
-
+	image = models.ImageField(default=False)
+	#Makes PostInfo Abstract 
 	class Meta:
 		abstract = True
 
+#Posts card to competition section of the site
+#inherits from PostInfo
+class Competition(PostInfo):
+	
+	#Different types of coding competitons 
+	#Used to organize and query database in the view
+	TYPE_OF_HACK = (
+		('Past Travel Hacks', 'Past Travel Hackathon'),
+		('Current Travel Hack','Current SoDA Travel Hackathon'),
+		('Past Offical SoDA Hack', 'Past Offical SoDA Coding Competiton'),
+		('Current Offical SoDA Hack','Current Offical SoDA Hackathon'),
+		('General Hacks','General Hackathons and Coding Competitons')
+		)
+	#Admin can pick what type of coding event 
+	competition_type = models.CharField(max_length=50,choices=TYPE_OF_HACK,default='General Hacks')
+#Updates the announcements page 
+class Announcement(PostInfo):
+	#True if post needs to be the headline
+	#Organized based on dates
+	headline = models.BooleanField()
