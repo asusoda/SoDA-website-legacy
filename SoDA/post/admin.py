7ae6b0f908b59cf648772e27fcda5006ee0bb200
Thomas F <taforres@asu.edu>
2015-07-27 18:20:16 -0700

    Changed admin header

--- SoDA/post/admin.py
+++ SoDA/post/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from post.models import Competition,Announcement
 
 # Register your models here.
-
+admin.site.site_header = "SoDA Admin Page!"
 admin.site.register(Competition)
 admin.site.register(Announcement)
