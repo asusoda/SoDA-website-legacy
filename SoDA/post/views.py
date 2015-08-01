18411b3e7300df52f7dede7e76febbee807e88aa
tforrest <taforres@asu.edu>
2015-07-31 22:22:19 -0700

    Removed is-active dictionary in views.py

--- SoDA/post/views.py
+++ SoDA/post/views.py
@@ -1,22 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from post.models import Announcement, Competition
-#For switch the highlight between tabs
-is_active = {'announcements':'mdl-layout__tab','welcome':'mdl-layout__tab','competitions':'mdl-layout__tab','calendar':'mdl-layout__tab','aboutus':'mdl-layout__tab','sponsors':'mdl-layout__tab'}
 
 # Create your views here.
 def index(request):
 	context_dict = {}
-	context_dict.update(is_active)
-	context_dict['welcome'] = 'mdl-layout__tab is-active'
 	context_dict['current_page'] = "Welcome to SoDA!"
 	return render(request,'post/index.html',context_dict)
 
 def announcements(request):
 	#Holds the queries to be dislayed on the front end
 	context_dict = {}
-	context_dict.update(is_active)
-	context_dict['announcements'] = 'mdl-layout__tab is-active'
 	context_dict['current_page'] = "Newsletter"
 	context_dict['news_post'] = Announcement.objects.exclude(headline=True).order_by('-date')
 
@@ -25,9 +19,7 @@ def announcements(request):
 def competitions(request):
 
 	context_dict = {}
-	context_dict.update(is_active)
 	context_dict['current_page'] = "Competitions"
-	context_dict['competitions'] = 'mdl-layout__tab is-active'
 	#holds the objects for future hackathons 
 	context_dict['future_hacks'] = Competition.objects.filter(competition_type='upcoming hack').order_by('-date')
 	#holds the objects for the past hackathons 
@@ -37,21 +29,16 @@ def competitions(request):
 
 def calendar(request):
 	context_dict = {}
-	context_dict.update(is_active)
 	context_dict['current_page'] = 'Calendar'
-	context_dict['calendar'] = 'mdl-layout__tab is-active'
 	return render(request,'post/calendar.html',context_dict)
 
 def aboutus(request):
 	context_dict = {}
-	context_dict.update(is_active)
+
 	context_dict['current_page'] = "About Us"
-	context_dict['aboutus'] = 'mdl-layout__tab is-active'
 	return render(request,'post/about-us.html',context_dict)
 
 def sponsors(request):
 	context_dict = {}
-	context_dict.update(is_active)
-	context_dict['sponsors'] = 'mdl-layout__tab is-active'
 	context_dict['current_page'] = 'Sponsors'
 	return render(request,'post/sponsors.html',context_dict)
