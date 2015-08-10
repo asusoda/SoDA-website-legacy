1af704b2e12346b9a7ce8b4ff162bd733f8d75e5
tforrest <taforres@asu.edu>
2015-08-09 21:55:30 -0700

    open and close cards using button

--- SoDA/static/js/hideshow.js
+++ SoDA/static/js/hideshow.js
@@ -1,10 +1,36 @@
 //script for hiding and showing cards/posts
-
-
-
+/*
+function hideShow() {
+  $(".mdl-button").click(function() {
+    $(this).parent(".showMe").not(this).each(function() {
+      console.log('first show me and rest removed')
+      $(this).removeClass("showMe").addClass("hideMe");
+    });
+    if($(this).parents('.hideMe').length) {
+      console.log('remove hide for button press')
+      $(this).parent('hideMe').removeClass('hideMe').addClass('showMe').hide.fadeIn(500);
+    }
+  });
+}
+*/
 $(document).ready(function() {
+ console.log('on ready test');
+ $('.hideMe').first().removeClass('hideMe').addClass('showMe');
+  $(".mdl-button").click(function() { 
+    $(".mdl-button").parent().siblings('.mdl-card__supporting-text').children('.showMe').not(this).each(function() {
+       $(".mdl-button").parent().siblings('.mdl-card__supporting-text').children('.showMe').removeClass("showMe").addClass("hideMe");
+    });
+    if($(this).parent().siblings('.mdl-card__supporting-text').children('.hideMe').length) {
+      console.log('remove hide for button press');
+      $(this).parent().siblings('.mdl-card__supporting-text').children('.hideMe').removeClass('hideMe').addClass('showMe').hide.fadeIn(500);
+      $(this).children('.expand').addClass('dontShow');
+    }
+  });
+
+
+  /*
   $(".card").first().removeClass("hideMe").addClass("showMe");
-  $(".card").click(function() {
+  $(".mdl-button").click(function() {
     $('.showMe').not(this).each(function() {
       $(this).removeClass("showMe").addClass("hideMe");
     });
@@ -13,7 +39,7 @@ $(document).ready(function() {
    
     }
 
-  });
+  }); */
 })
 
 
