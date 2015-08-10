167ea04bb09a5e2552e5baff7033893d70c4a8da
tforrest <taforres@asu.edu>
2015-08-10 12:23:27 -0700

    removes button on card when clicked

--- SoDA/static/js/hideshow.js
+++ SoDA/static/js/hideshow.js
@@ -1,70 +1,18 @@
-//script for hiding and showing cards/posts
-/*
-function hideShow() {
-  $(".mdl-button").click(function() {
-    $(this).parent(".showMe").not(this).each(function() {
-      console.log('first show me and rest removed')
-      $(this).removeClass("showMe").addClass("hideMe");
-    });
-    if($(this).parents('.hideMe').length) {
-      console.log('remove hide for button press')
-      $(this).parent('hideMe').removeClass('hideMe').addClass('showMe').hide.fadeIn(500);
-    }
-  });
-}
-*/
+//script to expand and hide cards 
 $(document).ready(function() {
- console.log('on ready test');
  $('.hideMe').first().removeClass('hideMe').addClass('showMe');
+ $('.mdl-button').first().addClass('dontShow');
   $(".mdl-button").click(function() { 
+    $('html,main').animate({scrollTop: $(this).parent().siblings('.mdl-card__media').offset().top},1000);
     $(".mdl-button").parent().siblings('.mdl-card__supporting-text').children('.showMe').not(this).each(function() {
-       $(".mdl-button").parent().siblings('.mdl-card__supporting-text').children('.showMe').removeClass("showMe").addClass("hideMe");
+      $(".mdl-button").parent().siblings('.mdl-card__supporting-text').children('.showMe').removeClass("showMe").addClass("hideMe");
+      $(".mdl-button").removeClass('dontShow');
     });
     if($(this).parent().siblings('.mdl-card__supporting-text').children('.hideMe').length) {
-      console.log('remove hide for button press');
-      $(this).parent().siblings('.mdl-card__supporting-text').children('.hideMe').removeClass('hideMe').addClass('showMe').hide.fadeIn(500);
-      $(this).children('.expand').addClass('dontShow');
+      $(this).parent().siblings('.mdl-card__supporting-text').children('.hideMe').removeClass('hideMe').addClass('showMe');
+      $(this).addClass('dontShow');
     }
   });
-
-
-  /*
-  $(".card").first().removeClass("hideMe").addClass("showMe");
-  $(".mdl-button").click(function() {
-    $('.showMe').not(this).each(function() {
-      $(this).removeClass("showMe").addClass("hideMe");
-    });
-    if ($(this).hasClass("hideMe") ) {
-      $(this).removeClass("hideMe").addClass("showMe").hide().fadeIn(500);
-   
-    }
-
-  }); */
 })
 
 
-
-
-
-/*
-$(".card").click(function() {
-  
-  $(".showMore").css("display", "block");
-  $('.showMe').not(this).each(function() {
-    $(this).removeClass("showMe").addClass("hideMe");
-    $(this).find(".mdl-card__actions").css("display","none");
-  });
-  if ($(this).hasClass("hideMe") ) {
-    $(this).removeClass("hideMe").addClass("showMe").hide().fadeIn(500);
-    $(this).find('div').hasClass(".mdl-card__actions").css("display", "none");
-  }
-});
-//removes the find more tag when the card is expanded
-$(document).ready(function() {
-  $(".mdl-card__actions").each(function() {
-    if ($(this).children(".showMe")) {
-    	console.log("If past")
-      $(this).removeClass(".mdl-card__actions");
-    }
-  });
-}); */
