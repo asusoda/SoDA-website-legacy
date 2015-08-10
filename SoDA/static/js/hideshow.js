847098a643cbe7ef0cba6e442706fb943989b5f6
tforrest <taforres@asu.edu>
2015-08-10 13:27:57 -0700

    changed selector for button in hideshow.js and reformated the file

--- SoDA/static/js/hideshow.js
+++ SoDA/static/js/hideshow.js
@@ -1,18 +1,18 @@
 //script to expand and hide cards 
-$(document).ready(function() {
- $('.hideMe').first().removeClass('hideMe').addClass('showMe');
- $('.mdl-button').first().addClass('dontShow');
-  $(".mdl-button").click(function() { 
-    $('html,main').animate({scrollTop: $(this).parent().siblings('.mdl-card__media').offset().top},1000);
-    $(".mdl-button").parent().siblings('.mdl-card__supporting-text').children('.showMe').not(this).each(function() {
-      $(".mdl-button").parent().siblings('.mdl-card__supporting-text').children('.showMe').removeClass("showMe").addClass("hideMe");
-      $(".mdl-button").removeClass('dontShow');
+$(document).ready(function () {
+    $('.hideMe').first().removeClass('hideMe').addClass('showMe');
+    $('.card-button').first().addClass('dontShow');
+    $('.card-button').click(function () {
+        $('html,main').animate({
+            scrollTop: $(this).parent().offset().top
+        }, 1000);
+        $('.card-button').parent().siblings('.mdl-card__supporting-text').children('.showMe').not(this).each(function () {
+            $(".card-button").parent().siblings('.mdl-card__supporting-text').children('.showMe').removeClass("showMe").addClass("hideMe");
+            $(".card-button").removeClass('dontShow');
+        });
+        if ($(this).parent().siblings('.mdl-card__supporting-text').children('.hideMe').length) {
+            $(this).parent().siblings('.mdl-card__supporting-text').children('.hideMe').removeClass('hideMe').addClass('showMe');
+            $(this).addClass('dontShow');
+        }
     });
-    if($(this).parent().siblings('.mdl-card__supporting-text').children('.hideMe').length) {
-      $(this).parent().siblings('.mdl-card__supporting-text').children('.hideMe').removeClass('hideMe').addClass('showMe');
-      $(this).addClass('dontShow');
-    }
-  });
 })
-
-
