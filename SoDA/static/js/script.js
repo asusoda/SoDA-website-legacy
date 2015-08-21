79b00de7d550d634a91bc06c8cdd1c674267b455
tforrest <taforres@asu.edu>
2015-08-20 21:40:20 -0700

    Removed redundant css selector

--- SoDA/static/js/script.js
+++ SoDA/static/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.hideMe').first().removeClass('hideMe').addClass('showMe');
-    $('.card-button').first().addClass('dontShow');
+    $('.card-button').first().addClass('hideMe');
     $('.mdl-card__supporting-text').first().css('margin','18px');
 
     hideShow();
@@ -23,12 +23,12 @@ function hideShow() {
         $('.card-button').parent().siblings('.mdl-card__supporting-text').children('.showMe').not(this).each(function () {
             $(".card-button").parent().siblings('.mdl-card__supporting-text').children('.showMe').removeClass("showMe").addClass("hideMe");
             $('.card-button').parent().siblings('.mdl-card__supporting-text').css('margin','0px');           
-            $(".card-button").removeClass('dontShow');
+            $(".card-button").removeClass('hideMe');
         });
         if ($(this).parent().siblings('.mdl-card__supporting-text').children('.hideMe').length) {
             $(this).parent().siblings('.mdl-card__supporting-text').children('.hideMe').removeClass('hideMe').addClass('showMe');
             $(this).parent().siblings('.mdl-card__supporting-text').css('margin','18px');
-            $(this).addClass('dontShow');
+            $(this).addClass('hideMe');
         }
         $('main').animate({
             scrollTop: $(this).closest('section')[0].offsetTop - 100
